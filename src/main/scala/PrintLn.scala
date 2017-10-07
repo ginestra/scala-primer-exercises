15fc986a69978e48599c92a1bc78cd7e4bcc79d0
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-10-07 18:47:39 +0100

    Adding the println exercise

---
+++ src/main/scala/PrintLn.scala
@@ -0,0 +1,18 @@
+object PrintLn {
+  def main(args: Array[String]): Unit = {
+    println(
+      """Congratulations, you are the newest ruler of ancient Samaria, elected
+for a ten year term of office. Your duties are to dispense food, direct
+farming, and buy and sell land as needed to support your people. Watch
+out for rat infestations and the plague! Grain is the general currency,
+measured in bushels. The following will help you in your decisions:
+* Each person needs at least 20 bushels of grain per year to survive.
+* Each person can farm at most 10 acres of land.
+* It takes 2 bushels of grain to farm an acre of land.
+* The market price for land fluctuates yearly.
+Rule wisely and you will be showered with appreciation at the end of
+your term. Rule poorly and you will be kicked out of office!
+      """
+    )
+  }
+}
